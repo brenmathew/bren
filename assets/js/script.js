2cd64663d79ed378b1a22d145f37a2fc6b36bfda
Bren Mathew <brenmathew10@gmail.com>
2024-08-05 23:19:05 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,91 +97,96 @@ filterBtn.forEach(btn => {
 // ChatGPT
 document.addEventListener("DOMContentLoaded", function() {
   const projectLinks = document.querySelectorAll(".project-link");
+  const modalContainer = document.getElementById("project-modal");
+
+  if (!modalContainer) {
+    console.error("Modal not found.");
+    return;
+  }
 
   projectLinks.forEach(link => {
     link.addEventListener("click", function(event) {
       event.preventDefault();
       const projectItem = this.closest(".project-item");
+
       if (!projectItem) {
         console.error("Project item not found.");
         return;
       }
 
       const projectTitle = projectItem.querySelector(".project-title")?.textContent || "No title";
-      const modalContainer = document.getElementById("project-modal");
-
-      if (!modalContainer) {
-        console.error("Modal not found.");
-        return;
-      }
-
-      console.log("Opening modal for:", projectTitle);
-
       let modalDescription, newImageSrc, additionalContent, projectLink;
 
-      if (projectTitle === "Super Market sales analysis") {
-        modalDescription = "This project analyzes supermarket sales data to gain insights into product performance, customer behavior, and revenue trends.";
-        newImageSrc = "./assets/images/project-09.png";
-        additionalContent = `
-          <div class="blog-image">
-            <img src="./assets/images/project-11.png" alt="Additional Image" loading="lazy">
-          </div>
-          <p class="blog-description">Below are some following questions analyzed on this dataset:</p>
-          <p class="blog-description">• What are the most selling Products?</p>
-          <p class="blog-description">• Which are the top 10 Products according to Sales?</p>
-          <p class="blog-description">• Which Products have proved to be the most profitable?</p>
-          <p class="blog-description">• What category sold the most?</p>
-          <p class="blog-description">• Which category is the most Profitable?</p>
-          <p class="blog-description">• What are the most selling products in each subcategory?</p>
-          <p class="blog-description">• Which customer segments are the most profitable?</p>
-          <p class="blog-description">• Which Shipping modes sold the most products?</p>
-          <p class="blog-description">• What markets sold the most Products?</p>
-          <p class="blog-description">• Which are the top 10 countries by sales?</p>
-          <p class="blog-description">• What's the average shipping cost for the top 10 different countries?</p>
-          <div class="blog-image">
-            <img src="./assets/images/project-12.png" alt="Another Image" loading="lazy">
-          </div>
-        `;
-        projectLink = "https://github.com/brenmathew/Data-Analysis/blob/main/SuperMarketSales_Analysis.ipynb";
-      } else if (projectTitle === "Netflix") {
-        modalDescription = "The following is a Netflix Analysis of various TV Shows and Movies acquired from a dataset through Kaggle.
-          We can see the description and genres of movies all at once through this dashboard.";
-        newImageSrc = "./assets/images/project-20.png";
-        additionalContent = `
-          <div class="blog-image">
-            <img src="./assets/images/project-21.png" alt="Additional Image" loading="lazy">
-          </div>
-          <p class="blog-description">This project is for Netflix. Below are the key insights:</p>
-          <p class="blog-description">• Analyze TV shows and movie genres</p>
-          <p class="blog-description">• Interactive dashboard to filter data</p>
-          <p class="blog-description">• Comprehensive overview of Netflix's content library</p>
-          <div class="blog-image">
-            <img src="./assets/images/project-22.png" alt="Another Image" loading="lazy">
-          </div>
-        `;
-        projectLink = "https://public.tableau.com/app/profile/brenmathew/viz/NetflixAnalysis_16769869059570/Netflix";
-      } else if (projectTitle === "Fabric Defect Detection") {
-        modalDescription = "The approach used here is based on image segmentation, which involves partitioning the image into different regions based on their visual characteristics.";
-        newImageSrc = "./assets/images/project-40.png";
-        additionalContent = `
-          <div class="blog-image">
-            <img src="./assets/images/project-41.png" alt="Additional Image" loading="lazy">
-          </div>
-          <p class="blog-description">Benefits:</p>
-          <p class="blog-description">• Quality Control in Textile Industry</p>
-          <p class="blog-description">• Efficient Fabric Inspection</p>
-          <p class="blog-description">• Reduced Fabric Waste</p>
-          <p class="blog-description">• Improved Product Quality</p>
-          <div class="blog-image">
-            <img src="./assets/images/project-42.png" alt="Another Image" loading="lazy">
-          </div>
-        `;
-        projectLink = "https://github.com/brenmathew/Fabric_Defect_detection";
-      } else {
-        modalDescription = `Detailed description of ${projectTitle}.`;
-        newImageSrc = projectItem.querySelector("img")?.src || "";
-        additionalContent = "";
-        projectLink = "https://github.com/brenmathew";
+      switch (projectTitle) {
+        case "Super Market sales analysis":
+          modalDescription = "This project analyzes supermarket sales data to gain insights into product performance, customer behavior, and revenue trends.";
+          newImageSrc = "./assets/images/project-09.png";
+          additionalContent = `
+            <div class="blog-image">
+              <img src="./assets/images/project-11.png" alt="Additional Image" loading="lazy">
+            </div>
+            <p class="blog-description">Below are some following questions analyzed on this dataset:</p>
+            <p class="blog-description">• What are the most selling Products?</p>
+            <p class="blog-description">• Which are the top 10 Products according to Sales?</p>
+            <p class="blog-description">• Which Products have proved to be the most profitable?</p>
+            <p class="blog-description">• What category sold the most?</p>
+            <p class="blog-description">• Which category is the most Profitable?</p>
+            <p class="blog-description">• What are the most selling products in each subcategory?</p>
+            <p class="blog-description">• Which customer segments are the most profitable?</p>
+            <p class="blog-description">• Which Shipping modes sold the most products?</p>
+            <p class="blog-description">• What markets sold the most Products?</p>
+            <p class="blog-description">• Which are the top 10 countries by sales?</p>
+            <p class="blog-description">• What's the average shipping cost for the top 10 different countries?</p>
+            <div class="blog-image">
+              <img src="./assets/images/project-12.png" alt="Another Image" loading="lazy">
+            </div>
+          `;
+          projectLink = "https://github.com/brenmathew/Data-Analysis/blob/main/SuperMarketSales_Analysis.ipynb";
+          break;
+
+        case "Netflix":
+          modalDescription = "The following is a Netflix Analysis of various TV Shows and Movies acquired from a dataset through Kaggle. We can see the description and genres of movies all at once through this dashboard.";
+          newImageSrc = "./assets/images/project-20.png";
+          additionalContent = `
+            <div class="blog-image">
+              <img src="./assets/images/project-21.png" alt="Additional Image" loading="lazy">
+            </div>
+            <p class="blog-description">This project is for Netflix. Below are the key insights:</p>
+            <p class="blog-description">• Analyze TV shows and movie genres</p>
+            <p class="blog-description">• Interactive dashboard to filter data</p>
+            <p class="blog-description">• Comprehensive overview of Netflix's content library</p>
+            <div class="blog-image">
+              <img src="./assets/images/project-22.png" alt="Another Image" loading="lazy">
+            </div>
+          `;
+          projectLink = "https://public.tableau.com/app/profile/brenmathew/viz/NetflixAnalysis_16769869059570/Netflix";
+          break;
+
+        case "Fabric Defect Detection":
+          modalDescription = "The approach used here is based on image segmentation, which involves partitioning the image into different regions based on their visual characteristics.";
+          newImageSrc = "./assets/images/project-40.png";
+          additionalContent = `
+            <div class="blog-image">
+              <img src="./assets/images/project-41.png" alt="Additional Image" loading="lazy">
+            </div>
+            <p class="blog-description">Benefits:</p>
+            <p class="blog-description">• Quality Control in Textile Industry</p>
+            <p class="blog-description">• Efficient Fabric Inspection</p>
+            <p class="blog-description">• Reduced Fabric Waste</p>
+            <p class="blog-description">• Improved Product Quality</p>
+            <div class="blog-image">
+              <img src="./assets/images/project-42.png" alt="Another Image" loading="lazy">
+            </div>
+          `;
+          projectLink = "https://github.com/brenmathew/Fabric_Defect_detection";
+          break;
+
+        default:
+          modalDescription = `Detailed description of ${projectTitle}.`;
+          newImageSrc = projectItem.querySelector("img")?.src || "";
+          additionalContent = "";
+          projectLink = "https://github.com/brenmathew";
+          break;
       }
 
       // Populate modal with specific project details
@@ -198,35 +203,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
       const closeButton = modalContainer.querySelector(".portfolio-modal-close-btn");
       closeButton.onclick = function() {
-        modalContainer.classList.remove("active"); // Hide the modal
+        modalContainer.classList.remove("active");
         window.removeEventListener("click", outsideClickListener);
       };
 
-      // Define the outside click listener
       const outsideClickListener = function(event) {
-        const isClickInside = modalContainer.querySelector(".portfolio-modal").contains(event.target);
-        if (!isClickInside && modalContainer.classList.contains("active")) {
-          modalContainer.classList.remove("active"); // Hide the modal
-          window.removeEventListener("click", outsideClickListener); // Remove the listener after hiding
+        if (!modalContainer.querySelector(".portfolio-modal").contains(event.target)) {
+          modalContainer.classList.remove("active");
+          window.removeEventListener("click", outsideClickListener);
         }
       };
 
-      // Attach the event listener
       setTimeout(() => {
         window.addEventListener("click", outsideClickListener);
-      }, 0); // Timeout ensures it does not trigger immediately after opening
+      }, 0);
 
-      // Stop propagation on modal content to prevent closing the modal
       modalContainer.querySelector(".portfolio-modal").addEventListener("click", function(event) {
         event.stopPropagation();
       });
     });
   });
 });
-
-
-
-
 
 
 // Page navigation variables
